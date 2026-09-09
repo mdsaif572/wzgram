33429ecea16d8c58cb82630a79ae60bdc4758383
mdsaif572 <143118236+mdsaif572@users.noreply.github.com>
2026-09-09 07:31:25 +0530

    perf: unlock 50-60+ MB/s download rate and 5 session pool

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -1453,20 +1453,20 @@ class Client(Methods):
                 _is_premium = self.me.is_premium if hasattr(self.me, 'is_premium') else False
 
                 if _is_bot:
-                    dl_pool_size = int(os.environ.get("WZGRAM_DL_POOL_BOT", 3))
-                    dl_workers_per_session = int(os.environ.get("WZGRAM_DL_WORKERS_BOT", 2))
-                    dl_rate = int(os.environ.get("WZGRAM_DL_RATE_BOT", 40))
-                    dl_burst = int(os.environ.get("WZGRAM_DL_BURST_BOT", 10))
+                    dl_pool_size = int(os.environ.get("WZGRAM_DL_POOL_BOT", 5))
+                    dl_workers_per_session = int(os.environ.get("WZGRAM_DL_WORKERS_BOT", 3))
+                    dl_rate = int(os.environ.get("WZGRAM_DL_RATE_BOT", 100))
+                    dl_burst = int(os.environ.get("WZGRAM_DL_BURST_BOT", 25))
                 elif _is_premium:
-                    dl_pool_size = int(os.environ.get("WZGRAM_DL_POOL_PREMIUM", 4))
-                    dl_workers_per_session = int(os.environ.get("WZGRAM_DL_WORKERS_PREMIUM", 3))
-                    dl_rate = int(os.environ.get("WZGRAM_DL_RATE_PREMIUM", 70))
-                    dl_burst = int(os.environ.get("WZGRAM_DL_BURST_PREMIUM", 15))
+                    dl_pool_size = int(os.environ.get("WZGRAM_DL_POOL_PREMIUM", 6))
+                    dl_workers_per_session = int(os.environ.get("WZGRAM_DL_WORKERS_PREMIUM", 4))
+                    dl_rate = int(os.environ.get("WZGRAM_DL_RATE_PREMIUM", 150))
+                    dl_burst = int(os.environ.get("WZGRAM_DL_BURST_PREMIUM", 35))
                 else:
-                    dl_pool_size = int(os.environ.get("WZGRAM_DL_POOL_USER", 3))
-                    dl_workers_per_session = int(os.environ.get("WZGRAM_DL_WORKERS_USER", 2))
-                    dl_rate = int(os.environ.get("WZGRAM_DL_RATE_USER", 40))
-                    dl_burst = int(os.environ.get("WZGRAM_DL_BURST_USER", 10))
+                    dl_pool_size = int(os.environ.get("WZGRAM_DL_POOL_USER", 5))
+                    dl_workers_per_session = int(os.environ.get("WZGRAM_DL_WORKERS_USER", 3))
+                    dl_rate = int(os.environ.get("WZGRAM_DL_RATE_USER", 100))
+                    dl_burst = int(os.environ.get("WZGRAM_DL_BURST_USER", 25))
 
                 total_chunks = math.ceil((file_size - offset_bytes) / chunk_size)
                 pool_size = min(dl_pool_size, total_chunks)
